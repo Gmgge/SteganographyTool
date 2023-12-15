b5cd227145f5354fa0ddb509384a7e95569c7fa9
Gmgge <157505853@qq.com>
2023-12-15 15:42:12 +0800

    upgrade:优化LSB算法逻辑，提高处理速度

--- hide_algorithm/LSB.py
+++ hide_algorithm/LSB.py
@@ -1,77 +1,73 @@
+import cv2
+import numpy as np
 from hide_algorithm.base_algorithm import BaseAlgorithm
 
 
-class DataTooBig(Exception):
+class SteganographyException(Exception):
     pass
 
 
 class LSB(BaseAlgorithm):
-    """
-    无损压缩位图像隐写，2位LSB， 适用于png，tiff等
-    """
-
     def __init__(self):
-        self.mask = 3
-        self.carrier_mode = "RGBA"
+        self.maskONEValues = [1, 2, 4, 8, 16, 32, 64, 128]
+        self.maskZEROValues = [254, 253, 251, 247, 239, 223, 191, 127]
+        self.max_use_bit = 3  # 最多使用3位用于写入数据
 
     def check_carrier(self, carrier_data):
-        """
-        有效隐写载体检查
-        :param carrier_data: 隐写载体数据
-        :return: bool
-        """
         sign = False
-        if carrier_data.mode == self.carrier_mode:
+        if carrier_data is not None and isinstance(carrier_data, np.ndarray):
             sign = True
         return sign
 
-    def read(self, carrier_data):
-        """
-        按照指针依次读取数据
-        :carrier_data: 载体图像数据 PIL ImageFile
-        :return: 成功返回数据，未成功读取返回None
-        """
-        read_ptr = 0
-        max_len = carrier_data.height * carrier_data.width
-        pixels = list(carrier_data.getdata())
-        while read_ptr < max_len:
-            r, g, b, a = pixels[read_ptr]
-            read_info = (r & self.mask) | ((g & self.mask) << 2) | ((b & self.mask) << 4) | ((a & self.mask) << 6)
-            yield read_info
-            read_ptr += 1
+    def read(self, carrier_data, ):
+        carrier_flat = carrier_data.flatten()
+        for use_bit_ptr in range(self.max_use_bit):
+            mask_one = self.maskONEValues[use_bit_ptr]
+            for i in range(len(carrier_flat))[::8]:
+                pre_pixel = (carrier_flat[i:i + 8] & mask_one).astype(str)
+                byte_data = ''.join(pre_pixel)
+                byte_data = int(byte_data, 2)
+                yield byte_data
 
     def hide(self, carrier_data, hide_data):
         """
-        在位图像中隐写数据，图像会被转换成四通道，每个像素写入1bits信息
-        :param carrier_data: 载体图像数据 PIL ImageFile
-        :param hide_data: 待隐写的二进制数据
-        :return:
+        将bytes数据隐写到矩阵数据中
+        :param carrier_data: numpy.array 载体矩阵
+        :param hide_data: bytes 待隐写消息
+        :return: numpy.array 隐写后的载体矩阵
         """
-        max_len = carrier_data.height * carrier_data.width
-        carrier_data = carrier_data.convert("RGBA")
-        pixels = list(carrier_data.getdata())
-        if len(hide_data) > max_len:
-            raise DataTooBig()
-        for i, byte in enumerate(hide_data):
-            r, g, b, a = pixels[i]
-            r = (r & ~self.mask) | (byte & self.mask)
-            g = (g & ~self.mask) | ((byte >> 2) & self.mask)
-            b = (b & ~self.mask) | ((byte >> 4) & self.mask)
-            a = (a & ~self.mask) | ((byte >> 6) & self.mask)
-            pixels[i] = (r, g, b, a)
-        carrier_data.putdata(pixels)
-        return carrier_data
+        # 备份载体信息
+        bak_shape = carrier_data.shape
+        carrier_flat = carrier_data.flatten()
+        # 检查载体容量
+        pixel_size = carrier_flat.size
+        binary_hide_str = "".join(format(byte, "08b") for byte in hide_data)
+        hide_len = len(binary_hide_str)
+        if pixel_size * self.max_use_bit < hide_len:
+            raise SteganographyException("Carrier image not big enough to hold all the data to steganography")
+
+        # 隐写信息分批
+        hide_list = [binary_hide_str[i:i + pixel_size] for i in range(0, hide_len, pixel_size)]
+        for i, hide_data in enumerate(hide_list):
+            hide_data = np.array(list(hide_data))
+            mask_one = self.maskONEValues[i]
+            mask_zero = self.maskZEROValues[i]
+            hide_carrier_flat = carrier_flat[:hide_data.size]
+            hide_carrier_flat[hide_data == "1"] = hide_carrier_flat[hide_data == "1"] | mask_one
+            hide_carrier_flat[hide_data == "0"] = hide_carrier_flat[hide_data == "0"] & mask_zero
+        hide_carrier = carrier_flat.reshape(bak_shape)
+        return hide_carrier
 
 
 if __name__ == "__main__":
-    import PIL.Image
-
-    img_in = PIL.Image.open(r"D:\py_project\steganography_extraction\data\0.png")
-    sto_opt = LSB()
-    # 写入
+    steg = LSB()
     hide_info = b"a"
-    test_data = sto_opt.hide(img_in, hide_info)
+    test_carrier = cv2.imread("../data/0.png")
+    img_encoded = steg.hide(test_carrier, hide_info)
+    cv2.imwrite("0_new.png", img_encoded)
 
+    # decoding
+    test_carrier = cv2.imread("0_new.png")
     # 读取
-    test_read = next(sto_opt.read(test_data))
-    print(test_read)
+    read_opt = steg.read(test_carrier)
+    print(next(read_opt))
